4625cea8c9047d21cdf38e1fff6ae36a913bcdc0
Corentin Le Bail <corentin@kyxar.fr>
2025-10-20 14:29:23 +0000

    Ok pour SVG

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,15 @@
 #include <QVBoxLayout>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "MaxRectsBinPack.h"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
     setupUi(this);
 
     svgWidget = new QSvgWidget(this);
-    svgWidget->load(QString("<?xml version='1.0'?><svg xmlns='http://www.w3.org/2000/svg'  version='1.2' baseProfile='tiny' width='120' height='120'><rect x='10' y='10' wudth='100' height='100' /></svg").toLocal8Bit());
-    svgWidget->show();
+    svgWidget->load(QString("<svg width=\"150\" height=\"250\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\"><rect x='10' y='10' width='100' height='100' /></svg>").toLocal8Bit());
+    // svgWidget->load(QString("/home/corentin/dev/RectangleBinPack/test.svg"));
 
     QVBoxLayout * verticalLayout = new QVBoxLayout(centralWidget()); // make a layout to put the svgwidget it so it scales with the window
     verticalLayout->setContentsMargins(0, 0, 0, 0); // no borders
